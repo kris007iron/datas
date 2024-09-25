65294d1594e3e7b2c9392d9b843291a0bda392e1
Krzysztof Mrugała <66689589+kris007iron@users.noreply.github.com>
2024-09-26 00:04:15 +0200

    struct and error types of matrix

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,1 +1,12 @@
-pub struct Matrix {}
+#[derive(Debug, PartialEq)]
+pub enum MatrixError {
+    InconsistentColumnSizes,
+    DimensionMismatch,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Matrix<T> {
+    data: Vec<Vec<T>>,
+    rows: u64,
+    cols: u64,
+}
