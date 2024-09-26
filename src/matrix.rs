ef8b71b49701ab43b9cddcafc1bfde0e7d291b5e
Krzysztof Mrugała <66689589+kris007iron@users.noreply.github.com>
2024-09-26 21:30:12 +0200

    addition with simple test

--- src/matrix.rs
+++ src/matrix.rs
@@ -28,4 +28,38 @@ impl Matrix<i64> {
 
         Ok(Self { data, rows, cols })
     }
+    pub fn add(&mut self, matrix: &Matrix<i64>) -> Result<(), MatrixError> {
+        if self.cols != matrix.cols || self.rows != matrix.rows {
+            return Err(MatrixError::DimensionMismatch);
+        }
+        let self_data_iterator = self.data.iter_mut();
+        let matrix_data_iterator = matrix.data.iter();
+        for (self_row, matrix_row) in self_data_iterator.zip(matrix_data_iterator) {
+            for (self_el, matrix_el) in self_row.iter_mut().zip(matrix_row) {
+                *self_el += matrix_el;
+            }
+        }
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn imatrix_addition() {
+        let mut matrix = match Matrix::<i64>::new(vec![vec![1, 2, 3], vec![1, 2, 3], vec![1, 2, 3]])
+        {
+            Ok(m) => m,
+            Err(e) => panic!("{:?}", e),
+        };
+
+        let matrix2 = match Matrix::<i64>::new(vec![vec![1, 2, 3], vec![2, 3, 1], vec![3, 1, 2]]) {
+            Ok(m) => m,
+            Err(e) => panic!("{:?}", e),
+        };
+        let res = matrix.add(&matrix2).err();
+        println!("{:?}", matrix);
+        assert_eq!(res, None);
+    }
 }
