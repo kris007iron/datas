16b912e38092f34d62b2f6ebe663f65ececdd53c
Krzysztof Mrugała <66689589+kris007iron@users.noreply.github.com>
2024-09-26 11:18:33 +0200

    constructor for i64 with error handeling for inconsistent column sizes in given data

--- src/matrix.rs
+++ src/matrix.rs
@@ -10,3 +10,22 @@ pub struct Matrix<T> {
     rows: u64,
     cols: u64,
 }
+
+impl Matrix<i64> {
+    pub fn new(data: Vec<Vec<i64>>) -> Result<Self, MatrixError> {
+        let rows = data.len() as u64;
+        if rows == 0 {
+            return Ok(Self {
+                data,
+                rows: 0,
+                cols: 0,
+            });
+        }
+        let cols = data[0].len() as u64;
+        if !data.iter().all(|row| row.len() as u64 == cols) {
+            return Err(MatrixError::InconsistentColumnSizes);
+        }
+
+        Ok(Self { data, rows, cols })
+    }
+}
